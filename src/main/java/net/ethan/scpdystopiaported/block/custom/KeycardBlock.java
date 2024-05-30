fd1cf49cd210c545e626edcf9125feeaabc18472
iSeeEthan <ethan.mcgee3@icloud.com>
2024-05-29 21:29:56 -0500

    .

--- src/main/java/net/ethan/scpdystopiaported/block/custom/KeycardBlock.java
+++ src/main/java/net/ethan/scpdystopiaported/block/custom/KeycardBlock.java
@@ -11,6 +11,7 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
+import net.minecraftforge.common.Tags;
 
 public class KeycardBlock extends Block {
     public KeycardBlock(Properties pProperties) {
